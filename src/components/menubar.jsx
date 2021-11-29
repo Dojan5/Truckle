09466af5ef5ad43daa0ca4b401b8d0d817457b18
Joel Lundborg <joel.lundborg@gmail.com>
2021-11-29 16:03:20 +0100

    Update default language

--- src/components/menubar.jsx
+++ src/components/menubar.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 export const Menubar = () => {
-    return null;
-    // return(
-    //     <div className="menubar">
-    //         Bap bap
-    //     </div>
-    // )
+    return(
+        <div className="menubar">
+            
+        </div>
+    )
 }
