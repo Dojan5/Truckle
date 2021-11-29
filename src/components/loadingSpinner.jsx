267792521787d6ad5e3117db5bd751f4e31c2e96
Joel Lundborg <joel.lundborg@gmail.com>
2021-11-29 18:15:26 +0100

    Fix stupid mistake adding localisations to the localisation loading fallback screen

--- src/components/loadingSpinner.jsx
+++ src/components/loadingSpinner.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 
-export const LoadingSpinner = () => {
-    const [t] = useTranslation();
-    return (
-        <div className="loading-spinner">
-            <div className="spinner">
+export const LoadingSpinner = () => (
+    <div className="loading-spinner">
+        <div className="spinner">
+            <span>
                 <span>
                     <span>
-                        <span>
-                            <span />
-                        </span>
+                        <span />
                     </span>
                 </span>
-
-            </div>
-            <span className="spinner-text">
-                {t("interface.loading")}
             </span>
+
         </div>
-    )
-}
+    </div>
+)
