22fd62a0e0bed994d09fd5fdae66c92e8046c6d5
Joel Lundborg <joel.lundborg@gmail.com>
2021-11-29 17:40:20 +0100

    Add proper menubar

--- src/components/menubar.jsx
+++ src/components/menubar.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useTranslation } from "react-i18next";
+import { CalculatorIcon, InformationIcon } from '../icons'; 
 
 export const Menubar = () => {
     const [t] = useTranslation();
 
-    return(
+    return (
         <nav className="app-menu">
-            <Link to="/">{t("interface.calculator")}</Link>
-            <Link to="/loading">Loading</Link>
-            <Link to="/about">{t("interface.about")}</Link>
+            <MenuButton to="/" icon={<CalculatorIcon />}>{t("interface.calculator")}</MenuButton>
+            <MenuButton to="/loading">Loading</MenuButton>
+            <MenuButton to="/about" icon={<InformationIcon />}>{t("interface.about")}</MenuButton>
         </nav>
+    )
+}
+
+const MenuButton = ({ children, to, icon }) => {
+    const location = useLocation();
+    const match = (location.pathname === to);
+    return (
+        <button className={match ? "menu-button active" : "menu-button"}>
+            {icon && icon}
+            <Link to={to}>
+                {children}
+            </Link>
+        </button>
     )
 }
---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+//Open source Remix Iconset from - https://remixicon.com/
+
+export * from './calculatorIcon';
+export * from './informationIcon';
